faeb2b06ddd1a05c182626725cecd70ce8ce126a
Sofía Egaña Jiménez <sofia.angaji@gmail.com>
2023-12-04 10:41:32 -0300

    rutas org y pet

--- routes/organization.py
+++ routes/organization.py
@@ -7,48 +7,127 @@ organization = Blueprint('/organizaciones', __name__)
 
 @organization.get('/organizaciones')
 def get_organizations():
-    all_organizations = Organization.query.all()
-    org_list = [
-        {       
-            'id' : organization.id,
-            'name' : organization.name,
-            'adress' : organization.adress,
-            'phone' : organization.phone,
-            'email' : organization.email,
-            'user' : organization.user,
-            'logo' : organization.logo,
-            'role' : organization.role
-        }
+    try:
+        all_organizations = Organization.query.all()
+        org_list = [
+            {       
+                'id' : organization.id,
+                'name' : organization.name,
+                'adress' : organization.adress,
+                'phone' : organization.phone,
+                'email' : organization.email,
+                'user' : organization.user,
+                'logo' : organization.logo,
+                'role' : organization.role
+            }
 
-        for organization in all_organizations
-    ]
+            for organization in all_organizations
+        ]
+        if not all_organizations:
+            return jsonify({'message': 'Todavía no hay organizaciones registradas'}), 404
+                    
 
-    return jsonify({'organizations': org_list})
+        else:
+            return jsonify({'organizations': org_list}) 
         
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
+        
+
+
 @organization.get('/organizaciones/<id>')
 def get_organization(id):
-    organization = Organization.query.get(id)
-    print(organization)
+    try:
+
+        organization = Organization.query.get(id)
+        
+        if organization:
+            return jsonify({'organization': org_schema.dump(organization)})
+
+        else:
+            return jsonify({'message': 'Organización no encontrada'}), 404 
     
-    if organization:
-        return org_schema.jsonify({organization})
-    else:
-        return jsonify({'message': 'Organización no encontrada'}), 404 
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+
 
 @organization.post('/organizaciones')
 def create_organization():
-    id = request.json['id'] 
-    name = request.json['name']
-    adress = request.json['adress']
-    phone = request.json['phone']
-    email = request.json['email'] 
-    user = request.json['user']
-    logo = request.json['logo'] 
-    role = request.json['role']
+    try:
+        name = request.form['name']
+        adress = request.form['adress']
+        phone = request.form['phone']
+        email = request.form['email'] 
+        user = request.form['user']
+        logo = request.form['logo'] 
+        role = request.form['role']
 
-    new_org = Organization(id, name, adress, phone, email, user, logo, role)
+        new_org = Organization(
+            name = name, 
+            adress = adress, 
+            phone = phone, 
+            email = email, 
+            user = user, 
+            logo = logo, 
+            role = role
+        )
 
-    db.session.add(new_org)
-    db.session.commit()
+        db.session.add(new_org)
+        db.session.commit()
 
-    return org_schema.dump(new_org),({})
+        return org_schema.dump(new_org),({'message': 'Organización registrada exitosamente'})
+    
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+
+
+# Eliminar organización por id
+@organization.delete('/organizaciones/<id>')
+def delete_organization(id):
+
+    try:
+        organization = Organization.query.get(id)
+
+        if not organization:
+            return jsonify({'message': 'Organizacion no encontrada'}), 404
+        
+        db.session.delete(organization)
+        db.session.commit()
+
+        return jsonify({'organization': org_schema.dump(organization), 'message': 'Organizacion eliminada exitosamente'}), 200
+
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+
+# versión no probada de UPDATE:
+
+@organization.put('/organizaciones/<id>')
+def update_organization(id):
+
+    #folder_img = current_app.config.get('FOLDER_IMG_PETS', 'public/images/pets')
+    try:
+        organization = Organization.query.get(id)
+
+        if not organization:
+            return jsonify({'message': 'Organizacion no encontrada'}), 404
+
+        # Usar request.json directamente en lugar de request.get_json()
+
+        data = request.form # request.son en este entorno?
+
+        # Iterar sobre las claves del diccionario y actualizar los atributos correspondientes
+        for key, value in data.items():
+            setattr(organization, key, value)
+        
+        db.session.commit()
+
+        # Devolver la representación JSON actualizada de la mascota
+        return jsonify({'organization': org_schema.dump(organization)})
+
+    except Exception as e:
+        # Manejar errores y devolver una respuesta JSON con un código de estado 500
+        return jsonify({'error': str(e)}), 500
--- routes/pet.py
+++ routes/pet.py
@@ -15,80 +15,88 @@ pets = Blueprint('/mascotas', __name__)
 # Obtener listado de mascotas
 @pets.get('/mascotas')
 def get_pets():
-    all_pets = Pet.query.all()
-    pets_list = [
-        {
-            'id': pet.id, 
-            'name': pet.name, 
-            'pet_type': pet.pet_type, 
-            'race': pet.race, 
-            'color': pet.color, 
-            'size': pet.size, 
-            'age': pet.age, 
-            'sex': pet.sex, 
-            'vaccine': pet.vaccine, 
-            'sterilization': pet.sterilization, 
-            'health_status': pet.health_status, 
-            'description': pet.description, 
-            'organization': pet.organization, 
-            'image': pet.image
-        } 
-        for pet in all_pets 
-    ]
+    try:
+        all_pets = Pet.query.all()
+        pets_list = [
+            {
+                'id': pet.id, 
+                'name': pet.name, 
+                'pet_type': pet.pet_type, 
+                'race': pet.race, 
+                'color': pet.color, 
+                'size': pet.size, 
+                'age': pet.age, 
+                'sex': pet.sex, 
+                'vaccine': pet.vaccine, 
+                'sterilization': pet.sterilization, 
+                'health_status': pet.health_status, 
+                'description': pet.description, 
+                'organization': pet.organization, 
+                'image': pet.image
+            } 
+            for pet in all_pets 
+        ]
 
-    return jsonify({'pets': pets_list})
+        if not all_pets:
+            return jsonify({'message': 'Todavía no tienes mascotas publicadas'}), 404
+                
+
+        else:
+            #print(pet.__dict__)
+            return jsonify({'pets': pets_list})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+           
+    
    
 # Obtener mascota por id
 @pets.get('/mascotas/<id>')
 def get_pet(id):
-    pet = Pet.query.get(id)
-    
-    print(pet)
-   
-    if pet:
-        print(pet.__dict__)
-        return jsonify({'pet': pet_schema.dump(pet)})
 
-    else:
-        return jsonify({'message': 'Mascota no encontrada'}), 404 
+    try:
+        pet = Pet.query.get(id)
     
-#Obtener mascotas por pet_type
-# def get_pets_by_type(pet_type):
-#     pets_of_type = Pet.query.filter_by(pet_type=pet_type).all()
+        if pet:
+            return jsonify({'pet': pet_schema.dump(pet)})
 
-#     if pets_of_type:
-#         pets_data = pet_schemas.dump(pets_of_type, many=True)
-#         return jsonify({'pets': pets_data})
-#     else:
-#         return jsonify({'message': 'No se encontraron mascotas de ese tipo'}), 404 
+        else:
+            return jsonify({'message': 'Mascota no encontrada'}), 404 
+
+    except Exception as e:
+        return jsonify({'error': str(e)})
+        
+    
+
 
 # Crear nueva mascota
 @pets.post('/mascotas')
 def create_pets():
-    folder_img = current_app.config.get('FOLDER_IMG_PETS', 'public/images/pets')
-    name = request.form['name']
-    pet_type = request.form['pet_type']
-    race = request.form['race']
-    color = request.form['color']
-    size = request.form['size']
-    sex = request.form['sex']
-    age = request.form['age']
-    vaccine = request.form['vaccine']
-    sterilization = request.form['sterilization']
-    health_status = request.form['health_status']
-    description = request.form['description']
-    organization = request.form['organization']
-    image = request.files['image']
+    try:
+        folder_img = current_app.config.get('FOLDER_IMG_PETS', 'public/images/pets')
+        name = request.form['name']
+        pet_type = request.form['pet_type']
+        race = request.form['race']
+        color = request.form['color']
+        size = request.form['size']
+        sex = request.form['sex']
+        age = request.form['age']
+        vaccine = request.form['vaccine']
+        sterilization = request.form['sterilization']
+        health_status = request.form['health_status']
+        description = request.form['description']
+        organization = request.form['organization']
+        image = request.files['image']
 
-    # Toma el nombre del archivo original como entrada y devuelve un nombre de archivo seguro para su almacenamiento.
-    image_add = secure_filename(image.filename)
+        # Toma el nombre del archivo original como entrada y devuelve un nombre de archivo seguro para su almacenamiento.
+        image_add = secure_filename(image.filename)
 
-    # Separa el nombre del archivo de su extensión, considerando el punto como separador.
-    name_base, extension = os.path.splitext(image_add)
-      
-
-    new_pet = Pet(
+        # Separa el nombre del archivo de su extensión, considerando el punto como separador.
+        name_base, extension = os.path.splitext(image_add)
         
+
+        new_pet = Pet(
+            
             name = name, 
             pet_type= pet_type, 
             race = race, 
@@ -102,71 +110,138 @@ def create_pets():
             description = description, 
             organization = organization, 
             image = f"/{folder_img }/{name_base}"
+            
+        )
         
-    )
+        db.session.add(new_pet)
+        db.session.commit()
+        
+        
+        # Obtiene el ID de la nueva mascota después de guardarla en la base de datos
+        new_pet_id = new_pet.id
+
+        # Construye el nombre de la imagen con el ID
+        name_image = f"pet_{new_pet_id}{extension}"
+
+        # Actualiza la ruta de la imagen con el nombre final
+        new_pet.image = f"/{folder_img}/{name_image}"
+
+        # Guarda la nueva mascota con la ruta de la imagen actualizada
+        db.session.commit()
+
+        # Guarda la imagen
+        image.save(os.path.join(folder_img, name_image))
+
+        return jsonify([pet_schema.dump(new_pet), {'message': 'Mascota creada exitosamente'}])
     
-    db.session.add(new_pet)
-    db.session.commit()
+    except Exception as e:
+        return jsonify({'error': str(e)})
     
-    
-     # Obtiene el ID de la nueva mascota después de guardarla en la base de datos
-    new_pet_id = new_pet.id
 
-    # Construye el nombre de la imagen con el ID
-    name_image = f"pet_{new_pet_id}{extension}"
-
-    # Actualiza la ruta de la imagen con el nombre final
-    new_pet.image = f"/{folder_img}/{name_image}"
-
-    # Guarda la nueva mascota con la ruta de la imagen actualizada
-    db.session.commit()
-
-    # Guarda la imagen
-    image.save(os.path.join(folder_img, name_image))
-
-    return jsonify([pet_schema.dump(new_pet), {'message': 'Mascota creada exitosamente'}])
-    
 # Eliminar mascota por id
 @pets.delete('/mascotas/<id>')
 def delete_pet(id):
-    pet = Pet.query.get(id)
+    try:
+        pet = Pet.query.get(id)
 
-    if not pet:
-        return jsonify({'message': 'Mascota no encontrada'}), 404
-    
-    db.session.delete(pet)
-    db.session.commit()
-
-    return jsonify({'pet': pet_schema.dump(pet), 'message': 'Mascota eliminada exitosamente'}), 200
-
-# Modificar mascota
-@pets.put('/mascotas/<id>')
-def update_pet(id):
-    pet = Pet.query.get(id)
-
-    if not pet:
-         return jsonify({'message': 'Mascota no encontrada'}), 404
-    else:
-        #print("Datos de la solicitud:", request.json)
-
-        # pet.name = request.json.get('name', pet.name)
-        # pet.pet_type = request.json.get('pet_type', pet.pet_type)
-        # pet.race = request.json.get('race', pet.race)
-        # pet.color = request.json.get('color', pet.color)
-        # pet.size = request.json.get('size', pet.size)
-        # pet.sex = request.json.get('sex', pet.sex)
-        # pet.age = request.json.get('age', pet.age)
-        # pet.vaccine = request.json.get('vaccine', pet.vaccine)
-        # pet.sterilization = request.json.get('sterilization', pet.sterilization)
-        # pet.health_status = request.json.get('health_status', pet.health_status)
-        # pet.description = request.json.get('description', pet.description)
-        # pet.organization = request.json.get('organization', pet.organization)
-        # pet.image = request.json.get('image', pet.image)
+        if not pet:
+            return jsonify({'message': 'Mascota no encontrada'}), 404
         
-        data = request.get_json()
+        db.session.delete(pet)
+        db.session.commit()
+
+        return jsonify({'pet': pet_schema.dump(pet), 'message': 'Mascota eliminada exitosamente'}), 200
+    
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+
+@pets.put('/mascotas/<id>')
+def update_pet(id): 
+    try:
+        #folder_img = current_app.config.get('FOLDER_IMG_PETS', 'public/images/pets')
+        pet = Pet.query.get(id)
+
+        if not pet:
+            return jsonify({'message': 'Mascota no encontrada'}), 404
+
+        # Usar request.json directamente en lugar de request.get_json()
+
+        data = request.form
+
+        # Iterar sobre las claves del diccionario y actualizar los atributos correspondientes
+        for key, value in data.items():
+            setattr(pet, key, value)
+        
+        db.session.commit()
+
+        # Devolver la representación JSON actualizada de la mascota
+        return jsonify({'pet': pet_schema.dump(pet)})
+
+    except Exception as e:
+        # Manejar errores y devolver una respuesta JSON con un código de estado 500
+        return jsonify({'error': str(e)}), 500
+
+
+
+"""@pets.put('/mascotas/<id>')
+def update_pet(id):
+    folder_img = current_app.config.get('FOLDER_IMG_PETS', 'public/images/pets')
+    try:
+        pet = Pet.query.get(id)
+
+        if not pet:
+            return jsonify({'message': 'Mascota no encontrada'}), 404
+
+        # Usar request.json directamente en lugar de request.get_json()
+
+        data = {
+            'name': request.form['name'],
+            'pet_type': request.form['pet_type'],
+            'race': request.form['race'],
+            'color': request.form['color'],
+            'size': request.form['size'],
+            'sex': request.form['sex'],
+            'age': request.form['age'],
+            'vaccine': request.form['vaccine'],
+            'sterilization': request.form['sterilization'],
+            'health_status': request.form['health_status'],
+            'description': request.form['description'],
+            'organization': request.form['organization'],
+            'image': request.files['image']
+        }
+
+
+
+        # Iterar sobre las claves del diccionario y actualizar los atributos correspondientes
         for key, value in data.items():
             setattr(pet, key, value)
 
+        # Toma el nombre del archivo original como entrada y devuelve un nombre de archivo seguro para su almacenamiento.
+        image_add = secure_filename(image.filename)
+
+        # Separa el nombre del archivo de su extensión, considerando el punto como separador.
+        name_base, extension = os.path.splitext(image_add)
+
+        # Construye el nombre de la imagen con el ID
+        name_image = f"pet_{data.image}{extension}"
+
+        # Actualiza la ruta de la imagen con el nombre final
+        data.image = f"/{folder_img}/{name_image}"
+
+        data.image = data.image
+
+        data.image.save(os.path.join(folder_img, name_image))
+
         db.session.commit()
-       
-        return pet_schema.jsonify({'pet': pet_schema.dump(pet)})
+
+        # Devolver la representación JSON actualizada de la mascota
+        return jsonify({'pet': pet_schema.dump(pet)})
+
+    except Exception as e:
+        # Manejar errores y devolver una respuesta JSON con un código de estado 500
+        return jsonify({'error': str(e)}), 500
+
+    except Exception as e:
+        # Manejar errores y devolver una respuesta JSON con un código de estado 500
+        return jsonify({'error': TypeError(e)}), 500"""
